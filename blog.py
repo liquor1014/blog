318485aee30e4f1b2cc509ec773910f02d9d2e79
liquor1014 <1250633306@qq.com>
2020-03-07 12:35:42 +0800

    新增blog的删除，更新功能；并且在写博文的页面添加返回主页案件

--- blog.py
+++ blog.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 #初始化程序和数据库
@@ -21,21 +21,73 @@ class Blog(db.Model):
         self.title = title
         self.text = text
 
+    def __repr__(self):
+        return self.title + ":" + self.text
+
+
 # db.create_all()  #创建数据库文件和数据库表，但只需操作一次
 
 
 @app.route('/')
-def home():
+def home_blog():
     '''
     渲染首页HTML模板文件
     '''
     return render_template('home.html')
 
+#查询博文全部列表
 @app.route('/blogs',methods=['GET'])
-def list():
-    blogs = Blog.query.all() #查询博文全部列表
+def list_blog():
+    blogs = Blog.query.all()
     return  render_template('list.html',blogs = blogs)
 
+#创建blog文章
+@app.route('/blogs/create',methods=['GET','POST'])
+def write_blog():
+    if request.method == 'GET':
+        return render_template('write.html')
+    else:
+        title = request.form['title']   #request.from获取以post方式提交的数据
+        text = request.form['text']
 
+    #创建一个blog对象
+        blog = Blog(title = title , text = text)
+        db.session.add(blog)
+        db.session.commit()     # 必须提交才能生效
+        return redirect('/blogs')   # 创建完成之后重定向到博文列表页面
+
+#blog详情和删除
+@app.route('/blogs/<uid>', methods=['GET','DELETE'])
+def del_inquire_blog(uid):
+    if request.method == 'GET':
+        blog = Blog.query.filter_by(id =uid).first_or_404()
+
+        return render_template('query_blog.html', blog=blog)
+    elif request.method == 'DELETE':
+        blog = Blog.query.filter_by(id =uid).delete()
+        db.session.commit()
+        return 'ok'
+
+@app.route('/blogs/update/<id>',methods = ['GET', 'POST'])
+def update_note(id):
+    '''
+    更新博文
+    '''
+    if request.method == 'GET':
+        # 根据ID查询博文详情
+        blog = Blog.query.filter_by(id = id).first_or_404()
+        # 渲染修改笔记页面HTML模板
+        return render_template('update_blog.html',blog = blog)
+    else:
+        # 获取请求的博文标题和正文
+        title = request.form['title']
+        text = request.form['text']
+
+        # 更新博文
+        blog = Blog.query.filter_by(id = id).update({'title':title,'text':text})
+        # 提交才能生效
+        db.session.commit()
+        # 修改完成之后重定向到博文详情页面
+        return redirect('/blogs/{id}'.format(id = id))
 
 app.run()
